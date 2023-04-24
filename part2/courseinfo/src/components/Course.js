4e3c2db451ad105f8aaa2a2b10f6cd25294d7e4a
Quan Do <minhquand3@gmail.com>
2023-04-24 03:55:53 +0000

    Exercise 2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,35 @@
+const Header = ({ course }) => <h2>{course}</h2>;
+
+const Total = ({ sum }) => (
+  <p>
+    <b>Total of {sum} exercises</b>
+  </p>
+);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => {
+  const sum = course.parts.reduce((acc, cur) => acc + cur.exercises, 0);
+
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={sum} />
+    </>
+  );
+};
+
+export default Course;
