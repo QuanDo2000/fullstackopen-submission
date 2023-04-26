51e80442d3e87469020a7bf5bbd71416be9d450e
Quan Do <minhquand3@gmail.com>
2023-04-26 23:08:38 +0000

    Part 3a

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method);
+  console.log('Path:  ', req.path);
+  console.log('Body:  ', req.body);
+  console.log('---');
+  next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
 app.use(express.json());
+app.use(requestLogger);
+app.use(unknownEndpoint);
 
 let notes = [
   {
