e0c7f809dd09b459cc8969e657f41894bd3d97b2
Quan Do <minhquand3@gmail.com>
2023-04-27 00:09:06 +0000

    Exercise 3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(
+  morgan('tiny', {
+    skip: (req, res) => {
+      return req.method === 'POST';
+    },
+  })
+);
+app.use(
+  morgan(
+    (tokens, req, res) => {
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res),
+        'ms',
+        JSON.stringify(req.body),
+      ].join(' ');
+    },
+    {
+      skip: (req, res) => {
+        return req.method !== 'POST';
+      },
+    }
+  )
+);
 
 let persons = [
   {
