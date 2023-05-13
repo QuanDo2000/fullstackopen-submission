5025aac05c68d0f27f0f36c0b1e1d6477d1b76a8
Quan Do <minhquand3@gmail.com>
2023-05-14 03:49:18 +0700

    Part 5d

---
+++ part5/backend/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true, // unique: true is not a validator, but a so-called 'helper' from the mongoose-unique-validator package
+  },
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note',
+    },
+  ],
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash; // the passwordHash should not be revealed
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
