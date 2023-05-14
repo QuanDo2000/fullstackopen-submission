eec9a2c590d3587defbbfb1b9f0f6f32840659ce
Quan Do <minhquand3@gmail.com>
2023-05-14 17:44:15 +0700

    Exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { vote } from '../reducers/anecdoteReducer';
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === '') {
+      return anecdotes;
+    }
+    return anecdotes.filter((anecdote) => anecdote.content.includes(filter));
+  });
   const dispatch = useDispatch();
 
   const handleVote = (id) => {
