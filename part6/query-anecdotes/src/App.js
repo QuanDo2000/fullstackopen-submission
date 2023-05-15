357aa89ae6735da771c752050fee3b502ff13c61
Quan Do <minhquand3@gmail.com>
2023-05-15 18:53:10 +0700

    Exercise 6.24

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -2,9 +2,11 @@ import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
 import { getAnecdotes, updateAnecdote } from './requests';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useNotificationDispatch } from './NotificationContext';
 
 const App = () => {
   const queryClient = useQueryClient();
+  const dispatch = useNotificationDispatch();
 
   const updateAnecdoteMutation = useMutation(updateAnecdote, {
     onSuccess: () => {
@@ -15,6 +17,13 @@ const App = () => {
   const handleVote = (anecdote) => {
     updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
     console.log('vote');
+    dispatch({
+      type: 'SET_NOTIFICATION',
+      data: `you voted '${anecdote.content}'`,
+    });
+    setTimeout(() => {
+      dispatch({ type: 'CLEAR_NOTIFICATION' });
+    }, 5000);
   };
 
   const result = useQuery('anecdotes', getAnecdotes, {
--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,13 +1,25 @@
 import { useMutation, useQueryClient } from 'react-query';
 import { createAnecdote } from '../requests';
+import { useNotificationDispatch } from '../NotificationContext';
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient();
+  const dispatch = useNotificationDispatch();
 
   const newAnecdoteMutation = useMutation(createAnecdote, {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData('anecdotes');
       queryClient.setQueryData('anecdotes', [...anecdotes, newAnecdote]);
+    },
+    onError: (error) => {
+      console.log(error);
+      dispatch({
+        type: 'SET_NOTIFICATION',
+        data: 'Anecdote creation failed, anecdote too short, must have length 5 or more.',
+      });
+      setTimeout(() => {
+        dispatch({ type: 'CLEAR_NOTIFICATION' });
+      }, 5000);
     },
   });
 
@@ -16,6 +28,10 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
     newAnecdoteMutation.mutate({ content, votes: 0 });
+    dispatch({ type: 'SET_NOTIFICATION', data: `you created '${content}'` });
+    setTimeout(() => {
+      dispatch({ type: 'CLEAR_NOTIFICATION' });
+    }, 5000);
   };
 
   return (
